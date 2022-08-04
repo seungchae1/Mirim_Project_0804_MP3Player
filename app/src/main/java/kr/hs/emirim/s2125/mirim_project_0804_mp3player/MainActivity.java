7c71afa5cee737a43ef0b11d8dfbb11f77e22b4d
seungchae1 <s2125@e-mirim.hs.kr>
2022-08-04 10:18:29 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0804_mp3player/MainActivity.java
@@ -0,0 +1,15 @@
+package kr.hs.emirim.s2125.mirim_project_0804_mp3player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Device File Explorer -> sdcard -> upload -> mp3파일 넣기
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
